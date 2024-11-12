226aaae9e05e028d99c9ee317d17af8905143d52
SANJIV <sanjiv@SANJIVs-MacBook-Air.local>
2024-11-12 11:32:11 +0530

    env updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const static_URL = "http://localhost:4000/static/uploads/";
-export const API_URL = "http://localhost:4000/api";
+export const static_URL = "https://api.piystudio.com/static/uploads/";
+export const API_URL = "https://api.piystudio.com/api";
