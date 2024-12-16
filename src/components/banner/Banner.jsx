b289850870a4e2d0afd45716df10ab30b7d00b27
SANJIV <sanjiv@SANJIVs-MacBook-Air.local>
2024-12-16 11:41:26 +0530

    fix video playing issue in banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,12 +1,21 @@
+import { createEffect } from "solid-js";
 import { API_URL, static_URL } from "../../config/config";
 
 import axios from "axios";
 const Banner = () => {
+  let videoRef; // Reference for the video element
+
+  // Effect to handle video playback
+  createEffect(() => {
+    if (videoRef) {
+      videoRef.play(); // Start playing the video as soon as it's ready
+    }
+  });
   return (
     <div class="w-full md:h-[90vh] h-[50vw]  mb-[30px]">
       <div class="w-full md:h-[90vh] h-[50vw] rounded-[20px] mt-[30px] overflow-hidden ">
         <video
-          // ref={videoRef}
+          ref={videoRef}
           // width="600"
           height={250}
           autoPlay
--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,3 @@
 export const static_URL = "https://api.piystudio.com/static/uploads/";
+//("http://localhost:4000/static/uploads/"); // "https://api.piystudio.com/static/uploads/";
 export const API_URL = "https://api.piystudio.com/api";
